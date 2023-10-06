241bd79c84c7be54e79defdfe266fd08ed28d839
heyderacpi <141354703+heyderacpi@users.noreply.github.com>
2023-10-05 19:27:07 -0500

    Update searchBox.js

--- src/components/searchBox/searchBox.js
+++ src/components/searchBox/searchBox.js
@@ -1,16 +1,30 @@
-import React from "react";
-
+import React, { useState } from "react";
 import SearchIcon from "./searchIcon";
-
 import "./index.css";
 
-const SearchBox = () => {
+const SearchBox = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState("");
 
-  return(
-  <div className="search-box">
-    <SearchIcon />
-    <input type="text" placeholder="Busca en este sitio web" />
-  </div>
-)};
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(searchQuery);
+  };
+
+  return (
+    <div className="search-box">
+      <SearchIcon />
+      <input
+        type="text"
+        placeholder="Busca en este sitio web"
+        value={searchQuery}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleSearch}>Buscar</button>
+    </div>
+  );
+};
 
 export default SearchBox;
