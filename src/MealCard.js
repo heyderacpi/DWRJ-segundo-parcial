01433399718d6e2bd609b0926181301de99f29b6
heyderacpi <141354703+heyderacpi@users.noreply.github.com>
2023-10-05 19:23:55 -0500

    Update MealCard.js

--- src/MealCard.js
+++ src/MealCard.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function MealCard({ meal, onFetchMealDetails }) {
+  return (
+    <div className='meal-card' onClick={() => onFetchMealDetails(meal.idMeal)}>
+      <img src={meal.strMealThumb} alt={meal.strMeal} />
+      <h3>{meal.strMeal}</h3>
+    </div>
+  );
+}
+
+export default MealCard;
 
