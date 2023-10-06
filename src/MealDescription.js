07a41f3021fde24c7221a161b35a2d2ac79d4342
heyderacpi <141354703+heyderacpi@users.noreply.github.com>
2023-10-05 19:25:04 -0500

    Update MealDescription.js

--- src/MealDescription.js
+++ src/MealDescription.js
@@ -1,1 +1,27 @@
 
+import React from 'react';
+
+function MealDescription(props) {
+  const { mealDetails, setShowMealDescription } = props;
+
+  const handleBackClick = () => {
+    setShowMealDescription(false);
+  };
+
+  return (
+    <div className="meal-description">
+      <div className="meal-description-header">
+        <button onClick={handleBackClick}>Volver</button>
+      </div>
+      {mealDetails && (
+        <div>
+          <img src={mealDetails.strMealThumb} alt={mealDetails.strMeal} />
+          <h2>{mealDetails.strMeal}</h2>
+          <p>{mealDetails.strInstructions}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default MealDescription;
